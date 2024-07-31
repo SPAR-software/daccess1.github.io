cab7a2af129303067d0ed20cdeab23e903823a65
grannysbuds <b.kruchinkin@yandex.ru>
2024-07-31 22:30:08 +0300

    fix invite button russian text

--- scripts/preload.js
+++ scripts/preload.js
@@ -63,7 +63,7 @@ document.addEventListener('preload', async () => {
 });
 
 async function preload() {
-    const ver = 110;
+    const ver = 111;
 
     const scripts = [
         `/scripts/index.js?v=${ver}`,
