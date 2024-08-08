24a8553128bf11bf66e50d248bcd6de3195b2cb5
grannysbuds <b.kruchinkin@yandex.ru>
2024-08-08 13:19:40 +0300

    update bot urls

--- scripts/preload.js
+++ scripts/preload.js
@@ -63,7 +63,7 @@ document.addEventListener('preload', async () => {
 });
 
 async function preload() {
-    const ver = 115;
+    const ver = 116;
 
     const scripts = [
         `/scripts/index.js?v=${ver}`,
